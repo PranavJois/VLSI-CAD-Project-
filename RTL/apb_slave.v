2e44ed6ac8ba2a847883754c70fd314c3d616d97
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-01-14 22:00:44 +0530

    Update apb_slave.v

--- RTL/apb_slave.v
+++ RTL/apb_slave.v
@@ -1,28 +1,26 @@
-module apb_slave (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire [31:0] paddr,
-    input  wire [31:0] pwdata,
-    input  wire        pwrite,
-    input  wire        psel,
-    input  wire        penable,
-    output reg  [31:0] prdata,
-    output reg         pready
+module apb_slave(
+    input clk, rst,
+    input [31:0] paddr, pwdata,
+    input pwrite, psel, penable,
+    output reg pready,
+    output reg [31:0] prdata
 );
 
-    reg [31:0] reg0;
+reg [31:0] mem;
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            reg0 <= 0;
-            pready <= 0;
-        end else begin
-            pready <= 0;
-            if (psel && penable) begin
-                pready <= 1;
-                if (pwrite) reg0 <= pwdata;
-                else prdata <= reg0;
-            end
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        mem <= 0;
+        pready <= 0;
+    end else begin
+        pready <= 0;
+        if (psel && penable) begin
+            pready <= 1;
+            if (pwrite)
+                mem <= pwdata;
+            else
+                prdata <= mem;
         end
     end
+end
 endmodule
