19cddd2cf386380971f0dc7574d14affb5e6b827
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-01-14 12:50:19 +0530

    Update cdc_handshake_bus.v

--- RTL/cdc_handshake_bus.v
+++ RTL/cdc_handshake_bus.v
@@ -1,58 +1,56 @@
-module cdc_handshake_bus #(
-    parameter WIDTH = 32
-)(
-    input  wire              src_clk,
-    input  wire              src_rst,
-    input  wire [WIDTH-1:0]  src_data,
-    input  wire              src_valid,
-    output reg               src_ready,
+module cdc_handshake_ctrl (
+    input  wire src_clk,
+    input  wire src_rst,
+    input  wire src_req,
+    output reg  src_ready,
 
-    input  wire              dst_clk,
-    input  wire              dst_rst,
-    output reg  [WIDTH-1:0]  dst_data,
-    output reg               dst_valid,
-    input  wire              dst_ready
+    input  wire dst_clk,
+    input  wire dst_rst,
+    output reg  dst_valid,
+    input  wire dst_done
 );
 
-    reg req_src, ack_dst;
-    wire req_dst_sync, ack_src_sync;
+    reg req_src;
+    reg ack_dst;
 
-    bit_sync sreq(.clk(dst_clk), .rst(dst_rst), .d(req_src), .q(req_dst_sync));
-    bit_sync sack(.clk(src_clk), .rst(src_rst), .d(ack_dst), .q(ack_src_sync));
+    wire req_dst_sync;
+    wire ack_src_sync;
 
-    reg [WIDTH-1:0] buf;
+    bit_sync sync_req (.clk(dst_clk), .rst(dst_rst), .d(req_src), .q(req_dst_sync));
+    bit_sync sync_ack (.clk(src_clk), .rst(src_rst), .d(ack_dst), .q(ack_src_sync));
 
+    // Source domain
     always @(posedge src_clk or posedge src_rst) begin
         if (src_rst) begin
-            req_src   <= 0;
-            src_ready <= 1;
+            req_src   <= 1'b0;
+            src_ready <= 1'b1;
         end else begin
-            if (src_valid && src_ready) begin
-                buf <= src_data;
-                req_src <= 1;
-                src_ready <= 0;
+            if (src_req && src_ready) begin
+                req_src   <= 1'b1;
+                src_ready <= 1'b0;
             end else if (ack_src_sync) begin
-                req_src <= 0;
-                src_ready <= 1;
+                req_src   <= 1'b0;
+                src_ready <= 1'b1;
             end
         end
     end
 
+    // Destination domain
     always @(posedge dst_clk or posedge dst_rst) begin
         if (dst_rst) begin
-            dst_valid <= 0;
-            ack_dst   <= 0;
+            dst_valid <= 1'b0;
+            ack_dst   <= 1'b0;
         end else begin
-            if (req_dst_sync && !dst_valid) begin
-                dst_data <= buf;
-                dst_valid <= 1;
-            end
-            if (dst_valid && dst_ready) begin
-                dst_valid <= 0;
-                ack_dst <= 1;
+            if (req_dst_sync && !dst_valid)
+                dst_valid <= 1'b1;
+
+            if (dst_valid && dst_done) begin
+                dst_valid <= 1'b0;
+                ack_dst   <= 1'b1;
             end else begin
-                ack_dst <= 0;
+                ack_dst <= 1'b0;
             end
         end
     end
+
 endmodule
